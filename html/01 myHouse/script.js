26383159ce926cd94ef1b00d4637140386450399
Mark Ulrich <31822@ma-web.nl>
2021-11-17 11:42:00 +0100

    Updated repo

--- html/01 myHouse/script.js
+++ html/01 myHouse/script.js
@@ -6,3 +6,61 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+
+context.beginPath();
+context.lineWidth = "2";
+context.fillStyle = "#ff0000";
+context.moveTo(300,100);
+context.lineTo(700,200);
+context.lineTo(600,400);
+context.lineTo(200,300);
+context.closePath();
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.lineWidth = "2";
+context.fillStyle = "#808080";
+context.moveTo(200,300);
+context.lineTo(600,400);
+context.lineTo(600,600);
+context.lineTo(200,500);
+context.closePath();
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.lineWidth = "2";
+context.fillStyle = "#808080";
+context.moveTo(700,200);
+context.lineTo(800,300);
+context.lineTo(600,400);
+context.closePath();
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.lineWidth = "2";
+context.fillStyle = "#808080";
+context.moveTo(800,300);
+context.lineTo(800,500);
+context.lineTo(600,600);
+context.lineTo(600,400);
+context.closePath();
+context.stroke();
+context.fill();
+
+
+/* context.beginPath();
+context.lineWidth = "10";
+//context.strokeStyle = "#ffcc00";
+context.strokeStyle = "rgb(255,255,0)";
+context.fillStyle = "rgba(255,255,0,0.4)";
+context.moveTo(100,100);
+context.lineTo(300,300);
+context.lineTo(400,100);
+context.lineTo(300,0);
+context.closePath(); //Connects lines to complete figure
+context.stroke();
+context.fill(); //Fills figure with default black style */
